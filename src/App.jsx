7bec6a87613e034abbee9b7c22951e0b5ad8d05f
Shadowplayer118 <dhanielmalinao@gmail.com>
2024-08-05 22:58:06 +0800

    Finally can get characters for update

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,57 @@
 import { useState, useEffect } from 'react';
-import { Button, ButtonGroup, Modal } from 'react-bootstrap';
+import { Button, Modal } from 'react-bootstrap';
 
 const Clients = () => {
     const [client, setClient] = useState([]);
     const [loading, setLoading] = useState(true);
 
     const [showAddModal, setShowAddModal] = useState(false);
-
     const makeAddModalAppear = () => setShowAddModal(!showAddModal);
+
+    const [showUpdateModal, setShowUpdateModal] = useState(true);
+    const makeUpdateModalAppear = () => setShowUpdateModal(!showUpdateModal);
 
     const [clientName, setClientName] = useState("");
 
+    const [residency, setResidency] = useState("");
+
+    const [selectedClientId, setSelectedClientId] = useState(0);
+    const [selectedClient, setSelectedClient] = useState({
+                                                        id: 0,
+                                                        clientName : "",
+                                                        residency : ""
+                                                    })
+
     const getClients = async () => {
         const response = await fetch(
-            "http://localhost:5184/api/ClientApi/GetClients"
+            "http://localhost:5034/api/ClientApi/GetClients"
         );
         const result = await response.json()
         setClient(result)
         setLoading(false);
     }
 
+    const getClient = async (id) => {
+        console.log(id);
+        const response = await fetch(
+            "http://localhost:5034/api/ClientApi/GetClient?id="+id,
+        );
+       
+        const result = await response.json()
+        console.log(result)
+        setSelectedClient(result)
+        makeUpdateModalAppear();
+    }
+
     const saveClient = async () => {
         const dataToSend = {
-            "clientName": clientName,
-            "residency": "quite place"
+            "client_name": clientName,
+            "residency": residency
         }
 
+
         const response = await fetch(
-            "http://localhost:5184/api/ClientApi/saveclient",
+            "http://localhost:5034/api/ClientApi/SaveClient",
             {
                 method: "POST",
                 headers: {
@@ -41,10 +65,29 @@ const Clients = () => {
 
     }
 
+
+
+    const updateClient = async () => {
+       
+        const response = await fetch(
+            "http://localhost:5034/api/ClientApi/UpdateClient?Id="+selectedClientId,
+            {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(selectedClient)
+            }
+        );
+
+        getClients();
+        makeUpdateModalAppear();
+    }
+
     const DeleteClient = async (id) => {
 
         const response = await fetch(
-            "http://localhost:5184/api/ClientApi/deleteclient?Id="+id,
+            "http://localhost:5034/api/ClientApi/deleteclient?Id="+id,
             {
                 method: "DELETE",
             }
@@ -63,18 +106,55 @@ const Clients = () => {
 
     return (
         <>
+
             <Modal show={showAddModal} onHide={makeAddModalAppear}>
                 <Modal.Header closeButton>
                     new client info
                 </Modal.Header>
                 <Modal.Body>
+                    <label htmlFor="clientName">Client Name</label>
                     <input type="text"
                         value={clientName}
                         onChange={(e) => setClientName(e.target.value)}
                     />
+
+                    <label htmlFor="clientName">Residency</label>
+                    <input type="text"
+                        value={residency}
+                        onChange={(e) => setResidency(e.target.value)}
+                    />
                 </Modal.Body>
                 <Modal.Footer>
                     <Button onClick={saveClient}>Save client</Button>
+                </Modal.Footer>
+            </Modal>
+
+
+
+            <Modal show={showUpdateModal} onHide={makeUpdateModalAppear}>
+                <Modal.Header closeButton>
+                    update client info
+                </Modal.Header>
+                
+                <Modal.Body>
+                    <input type="text"
+                        value={selectedClient.client_name}
+                        onChange={(e) => 
+                            setSelectedClient((c)=> 
+                                ({...c, clientName : e.target.value}))
+                        }
+                    />
+
+                    <input type="text"
+                        value={selectedClient.residency}
+                        onChange={(e) => 
+                            setSelectedClient((c)=> 
+                                ({...c, clientName : e.target.value}))
+                        }
+                    />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button onClick={updateClient}>Update client</Button>
                 </Modal.Footer>
             </Modal>
 
@@ -87,13 +167,20 @@ const Clients = () => {
                 <ul>
                     {
                         client.map((c) =>
-                            <li key={c.id} >{c.clientName} || {c.residency}
+                            <li key={c.id} >{c.client_name} || {c.residency}
+                            <Button className='btn-warning' 
+                                onClick={()=> {
+                                    getClient(c.id);
+                                    setSelectedClientId(c.id);
+                                }}
+                            >Update</Button>
                             <Button onClick={()=>DeleteClient(c.id)}>Delete</Button>
                             </li>
                         )
                     }
                 </ul>
             </div>
+
         </>
     );
 }
